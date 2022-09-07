8a7e9996170c3ddac242f3deeac235ce5775d3da
임수연 <81746616+dlatndus@users.noreply.github.com>
2022-09-08 00:39:14 +0900

    카메라, 씬 수정

--- HIEARTH/Assets/Scripts/setResolution.cs
+++ HIEARTH/Assets/Scripts/setResolution.cs
@@ -2,82 +2,24 @@ using UnityEngine;
 
 public class setResolution : MonoBehaviour
 {
-
-    #region Pola
-    private int ScreenSizeX = 0;
-    private int ScreenSizeY = 0;
-    #endregion
-
-    #region metody
-
-    #region rescale camera
-    private void RescaleCamera()
+    void Awake()
     {
-
-        if (Screen.width == ScreenSizeX && Screen.height == ScreenSizeY) return;
-
-        float targetaspect = 16.0f / 9.0f;
-        float windowaspect = (float)Screen.width / (float)Screen.height;
-        float scaleheight = windowaspect / targetaspect;
         Camera camera = GetComponent<Camera>();
-
-        if (scaleheight < 1.0f)
+        Rect rect = camera.rect;
+        float scaleheight = ((float)Screen.width / Screen.height) / ((float)16 / 9); // (가로 / 세로)
+        float scalewidth = 1f / scaleheight;
+        if (scaleheight < 1)
         {
-            Rect rect = camera.rect;
-
-            rect.width = 1.0f;
             rect.height = scaleheight;
-            rect.x = 0;
-            rect.y = (1.0f - scaleheight) / 2.0f;
-
-            camera.rect = rect;
+            rect.y = (1f - scaleheight) / 2f;
         }
-        else // add pillarbox
+        else
         {
-            float scalewidth = 1.0f / scaleheight;
-
-            Rect rect = camera.rect;
-
             rect.width = scalewidth;
-            rect.height = 1.0f;
-            rect.x = (1.0f - scalewidth) / 2.0f;
-            rect.y = 0;
-
-            camera.rect = rect;
+            rect.x = (1f - scalewidth) / 2f;
         }
-
-        ScreenSizeX = Screen.width;
-        ScreenSizeY = Screen.height;
-    }
-    #endregion
-
-    #endregion
-
-    #region metody unity
-
-    void OnPreCull()
-    {
-        if (Application.isEditor) return;
-        Rect wp = Camera.main.rect;
-        Rect nr = new Rect(0, 0, 1, 1);
-
-        Camera.main.rect = nr;
-        GL.Clear(true, true, Color.black);
-
-        Camera.main.rect = wp;
-
+        camera.rect = rect;
     }
 
-    // Use this for initialization
-    void Start()
-    {
-        RescaleCamera();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        RescaleCamera();
-    }
-    #endregion
+    void OnPreCull() => GL.Clear(true, true, Color.black);
 }
